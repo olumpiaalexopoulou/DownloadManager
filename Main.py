175508024e49b114e4765f7470d9ceacc1124f13
NickLitharis <p19lith@ionio.gr>
2021-03-06 14:10:56 +0200

    functions added

--- Main.py
+++ Main.py
@@ -2,21 +2,21 @@ from __future__ import unicode_literals
 from Downloadmp3 import mp3Downloader
 from Downloadm4a import m4aDownloader
 from Move_Song import moveSongs
-from pathlib import Path
+from getPath import *
 import time
-import os
 
 
 # Main program function
 if __name__ == '__main__':
 
     print("Welcome!!!")
-    time.sleep(2)
-    url = 'https://www.youtube.com/watch?v=2UKTSEUL5kQ'
+    # time.sleep(1)
+
     #url = input("Enter the video url:")
 
     # Getting the user's choise
     choice = input("Choose .mp3 or .m4a [1] or [2]:")
+    url = 'https://www.youtube.com/watch?v=2UKTSEUL5kQ'
 
     if choice == "1":
         mp3Downloader(url)
@@ -24,8 +24,8 @@ if __name__ == '__main__':
         m4aDownloader(url)
 
     # Getting curent directory and future directory to move the song
-    sourcePath = os.getcwd()
-    destinationPath = str(Path.home() / "Downloads")
+    sourcePath = getsrcPath()
+    destinationPath = getdstPAth()
 
     moveSongs(sourcePath, destinationPath)
     print("Your file is at:", destinationPath)
