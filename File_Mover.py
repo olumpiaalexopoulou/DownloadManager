3a3b65ed5a6785c894e21cf1795b2a99c3964f9d
NickLitharis <p19lith@ionio.gr>
2021-03-07 16:36:48 +0200

    name changed

---
+++ File_Mover.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+from Rename_Song import remove_unwanted_letters
+
+
+def moveSongs(source, destination):
+    os.chdir(source)
+    content = os.listdir()  # list all files in current directory
+    for file in content:
+
+        # Searching for the .mp3 file, renaming it and moving it to the new directory
+        if file.endswith('.mp3'):
+            oldfile = file
+            file = remove_unwanted_letters(file)
+            src = os.path.join(source, oldfile)
+            dst = os.path.join(destination, file)
+            shutil.move(src, dst)
+
+        # Searching for the .m4a file thenn moving it to the new directory
+        if file.endswith('.m4a'):
+            oldfile = file
+            src = os.path.join(source, oldfile)
+            dst = os.path.join(destination, file)
+            shutil.move(src, dst)
+
+        if file.endswith('.mp4'):
+            oldfile = file
+            src = os.path.join(source, oldfile)
+            dst = os.path.join(destination, file)
+            shutil.move(src, dst)
