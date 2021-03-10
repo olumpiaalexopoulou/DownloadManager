382073199c7c7d611af8dbb7b6189fd675957e2a
NickLitharis <p19lith@ionio.gr>
2021-03-10 11:20:00 +0200

    lines deleted and stracture changed

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from __future__ import unicode_literals
-from Downloadmp3 import mp3Downloader
-from Downloadm4a import m4aDownloader
-from Downloadmp4 import mp4Downloader
-from File_Mover import moveSongs
-from getPath import *
-#import time
+from DownloadAudio import AudioDownloader
+from DownloadVideo import VideoDownloader
+from FileMover import moveSongs
+from GetPath import *
 
 
 # Main program function
 if __name__ == '__main__':
 
     print("Welcome!!!")
-    # time.sleep(1)
 
     #url = input("Enter the video url:")
 
@@ -19,16 +16,14 @@ if __name__ == '__main__':
     choice = input("Choose .mp3, .m4a or .mp4 [1] or [2] or [3]:")
     url = 'https://www.youtube.com/watch?v=_cKHn_tMukg'  # no copyright song
 
-    if choice == "1":
-        mp3Downloader(url)
-    elif choice == "2":
-        m4aDownloader(url)
+    if (choice == "1") or (choice == "2"):
+        AudioDownloader(url)
     else:
-        mp4Downloader(url)
+        VideoDownloader(url)
 
-    # Getting curent directory and destination directory to move the song
+    # Getting current directory and destination directory to move the song
     sourcePath = getsrcPath()
     destinationPath = getdstPath()
 
-    moveSongs(sourcePath, destinationPath)
+    moveSongs(sourcePath, destinationPath, choice)
     print("Your file is at:", destinationPath)
