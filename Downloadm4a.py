c1a6c332103d6cdddbcae24a1ee7340d75dec183
NickLitharis <p19lith@ionio.gr>
2021-03-10 11:15:38 +0200

    files deleted

--- Downloadm4a.py
+++
@@ -1,7 +0,0 @@
-import pafy
-
-
-def m4aDownloader(url):
-
-    # calling the 'new' method of pafy and downloading the best audio quality song
-    result = pafy.new(url).getbestaudio().download()
--- Downloadmp4.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-
-def mp4Downloader(url):
-
-    url = YouTube(url)
-    mp4 = url.streams.first()
-    mp4.download()
-    print("Your video is ready")
