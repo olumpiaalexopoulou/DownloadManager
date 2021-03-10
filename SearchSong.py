e35251c49dbf6b5147eb0c34548e67c81c83c86a
NickLitharis <p19lith@ionio.gr>
2021-03-10 21:06:22 +0200

    search bar added (not finished yet)

---
+++ SearchSong.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import urllib.request
+import urllib.parse
+import re
+
+
+def GetSongUrl(songname):
+    # Allazei ta kena me kato pavles gia na dexetai ton titlo o parakato algorithmos
+    search_keyword = songname.replace(" ", "_")
+
+    html = urllib.request.urlopen(
+        "https://www.youtube.com/results?search_query="+search_keyword)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    print("https://www.youtube.com/watch?v="+video_ids[0])
+
+
+GetSongUrl("take on me")
