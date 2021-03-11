40e6ef2a3af3af165daacf30e04eb6beb34ee8ff
NickLitharis <p19lith@ionio.gr>
2021-03-11 16:02:13 +0200

    support for custom playlists added

--- DownloadAudio.py
+++ DownloadAudio.py
@@ -5,5 +5,5 @@ def AudioDownloader(url):
 
     # Xrisimopoioyme to loop se periptosi poy yparxoun perissotera apo ena tragoudia
     # Me me tin methodo new kai thn getbestaudio katebazoume thn kalhterh poothta ixoy apo to YouTube
-    # for u in url:
-    result = pafy.new(url).getbestaudio().download()
+    for u in url:
+        result = pafy.new(u).getbestaudio().download()
