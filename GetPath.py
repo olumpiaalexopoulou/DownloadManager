35ef142c759c495c06e04811c1875fb14d3cb3ef
NickLitharis <p19lith@ionio.gr>
2021-03-12 20:38:18 +0200

    directory changes

--- GetPath.py
+++ GetPath.py
@@ -10,6 +10,6 @@ def getsrcPath():
 
 def getdstPath():
     # Epistrefei to teliko directory
-    save_directory = str(Path.home()/"Downloads")
-    #save_directory = filedialog.askdirectory()
+    #save_directory = str(Path.home()/"Downloads")
+    save_directory = filedialog.askdirectory()
     return save_directory
