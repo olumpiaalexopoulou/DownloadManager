ec217abc375252cf05b4f7528cedc55e3b6f8bdd
NickLitharis <p19lith@ionio.gr>
2021-03-13 13:43:36 +0200

    dropdown menu added

--- FrontMain.py
+++ FrontMain.py
@@ -1,4 +1,4 @@
-from tkinter import Frame,Tk, Entry,Button,NONE,filedialog,messagebox
+from tkinter import Frame,Tk, Entry,Button,NONE,filedialog,messagebox,StringVar,OptionMenu
 from BackMain import down
 from pathlib import Path
 import os
@@ -6,7 +6,15 @@ import os
 root = Tk()
 root.geometry('400x240')
 root.resizable(0, 0)
-root.title("Download manager")
+root.title("Download Manager")
+
+
+#Dropdown menu
+options=[
+    "mp3",
+    "m4a",
+    "mp4"
+]
 
 
 
@@ -15,41 +23,46 @@ def SearchWindow():
     w.place(x=0, y=28)
 
     # entrybox
-    '''
+    """
     def on_entry(e):
         s.delete(0, 'end')
 
     def on_leave(e):
         if s.get() == "":
             s.insert(0, "Search Song")
-    '''
+    """
 
     def searchbar():
         search=s.get()
         return search
 
     def filetypebar():
-        ch=o.get()
+        ch=click.get()
         return ch
 
     def calldown(dm,video_type,user_input):
         down(dm,video_type,user_input)
         
-
+    click=StringVar()
+    click.set(options[0])
+    drop=OptionMenu(w,click,*options)
+    drop.place(x=160,y=92)
 
 
 
     s = Entry(w, width=30, fg="grey")
-    #s.configure(font=("Consolas", 5, "bold"))
-    #s.bind("<FocusIn>", on_entry)
-    #s.bind("<FocusIn>", on_leave)
-    #s.insert(0, "Search Song")
+    """
+    s.configure(font=("Consolas", 10, "bold"))
+    s.bind("<FocusIn>", on_entry)
+    s.bind("<FocusIn>", on_leave)
+    s.insert(0, "Search Song")
+    """
     s.place(x=100, y=42)
     
 
     # entry box for options
-    o = Entry(w, width=30, fg="black")
-    o.place(x=100, y=92)
+    #o = Entry(w, width=30, fg="black")
+    #o.place(x=100, y=92)
 
     Button(w, width=20, height=0, text="Download", command=lambda:calldown('1',filetypebar(),searchbar()),
            border=0, bg='light blue', fg='white').place(x=120, y=142)
@@ -86,15 +99,19 @@ def LinkWindow():
         return search
 
     def filetypebar():
-        ch=o1.get()
+        ch=click1.get()
         return ch
 
     def calldown(dm,video_type,user_input):
         down(dm,video_type,user_input)
 
     # entry box for options
-    o1 = Entry(q, width=30, fg="black")
-    o1.place(x=100, y=92)
+    #o1 = Entry(q, width=30, fg="black")
+    #o1.place(x=100, y=92)
+    click1=StringVar()
+    click1.set(options[0])
+    drop=OptionMenu(q,click1,*options)
+    drop.place(x=160,y=92)
 
     Button(q, width=20, height=0, text="Download", command=lambda:calldown('1',filetypebar(),searchbar()),
            border=0, bg='light blue', fg='white').place(x=120, y=142)
@@ -103,24 +120,6 @@ def LinkWindow():
 def CustomPlaylist():
     c = Frame(root, width=400, height=240, bg='red')
     c.place(x=0, y=28)
-
-    # entrybox
-    '''
-    def on_entry(e):
-        cp.delete(0, 'end')
-
-    def on_leave(e):
-        if cp.get() == "":
-            cp.insert(0, "Search Song")
-    '''
-    #cp = Entry(c, width=30, fg="grey")
-    '''
-    cp.configure(font=("Consolas", 10, "bold"))
-    cp.bind("<FocusIn>", on_entry)
-    cp.bind("<FocusIn>", on_leave)
-    cp.insert(0, "Search Song")
-    '''
-    #cp.place(x=100, y=42)
     
     def openfile():
         c.songlist=filedialog.askopenfilename()
@@ -128,20 +127,23 @@ def CustomPlaylist():
         c.directory=p.parent
         return c.directory
         
-
-
     def filetypebar():
-        ch=o2.get()
+        ch=click2.get()
         return ch
 
     def calldown(dm,video_type,user_input):
         down(dm,video_type,user_input)
 
     # entry box for options
-    o2 = Entry(c, width=30, fg="black")
-    o2.place(x=100, y=92)
+    #o2 = Entry(c, width=30, fg="black")
+    #o2.place(x=100, y=92)
 
-    Button(c,width=20,height=0,text="Select File",command=lambda:openfile(),border=0,bg='orange',fg='white').place(x=100,y=42)
+    click2=StringVar()
+    click2.set(options[0])
+    drop=OptionMenu(c,click2,*options)
+    drop.place(x=160,y=92)
+
+    Button(c,width=20,height=0,text="Select File",command=lambda:openfile(),border=0,bg='orange',fg='white').place(x=120,y=42)
     Button(c, width=20, height=0, text="Download", command=lambda:calldown('3',filetypebar(),c.directory),
            border=0, bg='light blue', fg='white').place(x=120, y=142)
 
