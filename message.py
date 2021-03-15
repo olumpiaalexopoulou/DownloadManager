ec5a82a2cc410fcaea6207890d78fd07af4783f3
NickLitharis <p19lith@ionio.gr>
2021-03-15 14:49:59 +0200

    no changes

--- message.py
+++ message.py
@@ -1,10 +1,12 @@
 from tkinter import Tk
 from tkinter import messagebox
 
-message=Tk()
+message = Tk()
 message.eval('tk::PlaceWindow %s center' % message.winfo_toplevel())
 message.iconbitmap(r"Icon/wave.ico")
 message.withdraw()
 
+
 def display(destinationPath):
-    messagebox.showinfo('Friendly Remainder',"Your file is at: " + destinationPath)
+    messagebox.showinfo('Friendly Remainder',
+                        "Your file is at: " + destinationPath)
