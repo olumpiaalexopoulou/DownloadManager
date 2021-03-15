7756cd9d5fa8cf94b39ab66e6f35fb675f5c193e
NickLitharis <p19lith@ionio.gr>
2021-03-15 09:55:53 +0200

    file dialog became default

--- GetPath.py
+++ GetPath.py
@@ -11,6 +11,6 @@ def getsrcPath():
 
 def getdstPath():
     # Epistrefei to teliko directory
-    save_directory = str(Path.home()/"Downloads")
-    #save_directory = filedialog.askdirectory()
+    #save_directory = str(Path.home()/"Downloads")
+    save_directory = filedialog.askdirectory()
     return save_directory
