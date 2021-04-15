15c23ede6584627cbbbeb26ff4f61a19a05ecf5e
olumpiaalexopoulou <p19alex@ionio.gr>
2021-04-15 18:06:30 +0300

    checkpoint 3

--- Checkpoint/FileToSend.py
+++ Checkpoint/FileToSend.py
@@ -110,7 +110,7 @@ def SearchWindow():
 
     Button(w, width=20, height=0, text="Download", command=lambda: download_files('1', filetypebar(), searchbar()),
            border=0, bg='#ffffff', fg='#000000').place(x=120, y=142)
-           
+
 def CustomPlaylist():
     
     c = Frame(root, width=400, height=240, bg='#4da6ff')
