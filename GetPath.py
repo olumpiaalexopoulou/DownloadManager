f742b4673160391d992af150d2fe82a1bd5d224f
Nick Litharis <p19lith@ionio.gr>
2021-04-23 15:00:46 +0300

    save directory dialog

--- GetPath.py
+++ GetPath.py
@@ -10,6 +10,6 @@ def getsrcPath():
 
 def getdstPath():
     # Returns "save" directory
-    save_directory = str(Path.home()/"Downloads")
-    #save_directory = filedialog.askdirectory()
+    # save_directory = str(Path.home()/"Downloads")
+    save_directory = filedialog.askdirectory()
     return save_directory
